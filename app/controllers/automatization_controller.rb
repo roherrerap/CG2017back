7f920d79f1196122bb3925ee89733ea9c0c37b55
LAPTOP-B91FCI08\Noctis <roherrerap@unal.edu.co>
2017-10-15 08:02:55 -0500

    Update - Automatization

---
+++ app/controllers/automatization_controller.rb
@@ -0,0 +1,14 @@
+class AutomatizationController < ApplicationController
+
+  # GET /productquotes
+  def index
+    query = "SELECT users.firstname, users.lastname, users.email, quotes.state, quotes.id, productquotes.quantity, productquotes.param1, productquotes.param2, productquotes.param3, productquotes.param4, productquotes.param5 FROM users, quotes, productquotes WHERE users.id = quotes.user_id AND quotes.id = productquotes.quote_id AND quotes.state = false"
+    results = ActiveRecord::Base.connection.execute(query)
+    if results.present?
+      render json: results
+    else
+      render json: false
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :products
   resources :quoteunregistereds
   mount_devise_token_auth_for 'User', at: 'auth'
+  resources :automatization
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
